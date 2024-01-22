d6247046f0917b9b6b7b8dc3bb45f9e65f89b2fc
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-23 03:19:51 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 var http = require("http");
-http.createServer((function(e, o) {
-	o.write("I'm alive"), o.end()
+http.createServer((function(e, t) {
+	t.write("I'm alive"), t.end()
 })).listen(8080);
 const {
 	Client: Client
-} = require("discord.js-selfbot-v13"), replace = require("replace-in-file"), client = new Client({
+} = require("discord.js-infer"), client = new Client({
 	checkUpdate: !1
 });
-client.on("ready", (async () => {
-	console.log("ready!");
-	const e = {
-		files: "./node_modules/discord.js-selfbot-v13/src/util/Options.js",
-		from: [/os: 'Windows',/, /browser: 'Discord Client',/, /os_version: '10.0.19045',/, /client_version: '1.0.9023',/],
-		to: ["os: 'iOS',", "browser: 'Discord iOS',", "os_version: '17.2',", "client_version: '211.0',"]
-	};
-	try {
-		await replace(e), console.log("Options.js modified successfully")
-	} catch (e) {
-		console.error("Error modifying Options.js:", e)
-	}
+client.on("ready", (() => {
+	console.log(`Logged in as ${client.user.tag}`)
 })), client.login(process.env.token);
