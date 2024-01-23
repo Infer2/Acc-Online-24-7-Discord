8287d59da81a7d6ec440083e6e6e0015f49a66ac
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-24 05:27:01 +0630

    Update streaming2.js

--- streaming2.js
+++ streaming2.js
@@ -1,26 +1,16 @@
+var http = require("http");
+http.createServer((function(e, t) {
+	t.write("I'm alive"), t.end()
+})).listen(8080);
 const {
-	Client,
-	RichPresence
-} = require('discord.js-infer');
-const client = new Client();
-
-client.on('ready', async () => {
+	Client: Client,
+	RichPresence: RichPresence
+} = require("discord.js-infer"), client = new Client;
+client.on("ready", (async () => {
 	console.log(`${client.user.username} is ready!`);
-	const getExtendURL = await RichPresence.getExternal(
-		client,
-		'857843402807967776',
-		'https://media.discordapp.net/attachments/1033549666769449002/1107009612210765955/matches.png',
-	);
-
-	const status = new RichPresence()
-		.setType(1)
-		.setURL('https://www.youtube.com/watch?v=9SH_iSb3-HU')
-		.setName('Korepi')
-		.setStartTimestamp(Date.now())
-    .setAssetsSmallText('What.')
-		.setAssetsSmallImage(getExtendURL[0].external_asset_path);
+	const e = await RichPresence.getExternal(client, "857843402807967776", "https://media.discordapp.net/attachments/1033549666769449002/1107009612210765955/matches.png"),
+		t = (new RichPresence).setType(1).setURL("https://www.youtube.com/watch?v=9SH_iSb3-HU").setName("Korepi").setStartTimestamp(Date.now()).setAssetsSmallText("What.").setAssetsSmallImage(e[0].external_asset_path);
 	client.user.setPresence({
-		activities: [status]
-	});
-});
-client.login(process.env.token)
+		activities: [t]
+	})
+})), client.login(process.env.token);
