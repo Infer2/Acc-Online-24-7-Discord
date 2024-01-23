7623ba89751274d69c9220db1d51d09808c2c130
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-24 05:30:18 +0630

    Update streaming2.js

--- streaming2.js
+++ streaming2.js
@@ -9,7 +9,7 @@ const {
 client.on("ready", (async () => {
 	console.log(`${client.user.username} is ready!`);
 	const e = await RichPresence.getExternal(client, "857843402807967776", "https://media.discordapp.net/attachments/1033549666769449002/1107009612210765955/matches.png"),
-		t = (new RichPresence).setType(1).setURL("https://www.youtube.com/watch?v=9SH_iSb3-HU").setName("Korepi").setStartTimestamp(Date.now()).setAssetsSmallText("What.").setAssetsSmallImage(e[0].external_asset_path);
+		t = (new RichPresence).setType(1).setURL("https://www.youtube.com/watch?v=9SH_iSb3-HU").setDetails("Korepi").setName("Miss u :)").setStartTimestamp(Date.now()).setAssetsSmallText("What.").setAssetsSmallImage(e[0].external_asset_path);
 	client.user.setPresence({
 		activities: [t]
 	})
