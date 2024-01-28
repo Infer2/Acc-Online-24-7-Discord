bdeef4f673d1ac12a7613e517cc36cce096648cb
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-28 10:23:27 +0630

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,9 @@ http.createServer((function (e, t) {
   .listen(8080);
 const {
   Client: Client
-  , SpotifyRPC: SpotifyRPC
-} = require("discord.js-infer"), client = new Client;
-client.on("ready", (async () => {
-  console.log(`${client.user.username} is ready!`);
-  const e = new SpotifyRPC(client)
-    .setAssetsLargeImage("spotify:ab67616d0000b273bfe215e877f862b83a73d43b")
-    .setAssetsLargeText("Looped By Infer")
-    .setState("Bunny Phyoe")
-    .setDetails("Eain Mat yk Athet")
-    .setStartTimestamp(Date.now())
-    .setEndTimestamp(Date.now() + 2484e6)
-    .setSongId("46D3Luru5sb8ubCysvP3t7")
-    .setAlbumId("76VEzdifZ8jbjRRxbSMmUc");
-  client.user.setPresence({
-    activities: [e]
-  })
-})), client.login(process.env.token);
+} = require("discord.js-infer"), client = new Client({
+  checkUpdate: !1
+});
+client.on("ready", (() => {
+        console.log(`Logged in as ${client.user.tag}`)
+        
