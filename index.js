02f00dbdecc5d51a9d15e1bb4fbc5152538d9d82
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-30 12:57:37 +0630

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ http.createServer((function (e, t) {
     t.write("I'm alive"), t.end()
   }))
   .listen(8080);
-const {
-  Client: Client
-} = require("discord.js-infer"), client = new Client({
-  checkUpdate: !1
+
+const { Client } = require("discord.js-infer");
+const client = new Client({
+  checkUpdate: false
 });
-client.on("ready", (() => {
-        console.log(`Logged in as ${client.user.tag}`)
-        
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
