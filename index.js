8b5b4366cf89a8221249a4d95a0fc26aeab04da3
@Infer <96887546+Infernite@users.noreply.github.com>
2024-02-01 06:21:28 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var http = require("http");
-http.createServer((function (e, t) {
-    t.write("I'm alive"), t.end()
-  }))
-  .listen(8080);
-
-const { Client } = require("discord.js-selfbot-v13");
-const client = new Client({
-  checkUpdate: false
+http.createServer((function(e, t) {
+	t.write("I'm alive"), t.end()
+})).listen(8080);
+const {
+	Client: Client
+} = require("discord.js-infer"), client = new Client({
+	checkUpdate: !1
 });
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
+client.on("ready", (() => {
+	console.log(`Logged in as ${client.user.tag}`)
+})), client.login(process.env.token);
