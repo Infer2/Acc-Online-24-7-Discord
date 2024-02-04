b1c7b7bee1450c434fbb8df1853643307952a24a
@Infer <96887546+Infernite@users.noreply.github.com>
2024-02-05 04:11:26 +0630

    Update mlbb.js

--- mlbb.js
+++ mlbb.js
@@ -13,26 +13,40 @@ http.createServer((function(e, t) {
   .listen(8080);
 
 client.on("ready", (async () => {
-  console.log(`${client.user.username} is ready!`), t = () => {
+  console.log(`${client.user.username} is ready!`);
+  
+  let stars = 336;
+
+  const initialTimestamp = Date.now();
+
+  const detailsText = `Stars: ${stars}`;
+
+  const e = new RichPresence(client)
+    .setApplicationId("367827983903490050")
+    .setType("PLAYING")
+    .setName("Mobile Legends")
+    .setDetails(detailsText)
+    .setStartTimestamp(initialTimestamp)  // Set the initial startTimestamp
+    .setAssetsSmallImage("https://media.discordapp.net/attachments/1158477807618379908/1203806027326165082/246x0w.webp")
+    .addButton('Watch', 'https://youtu.be/2yJgwwDcgV8')
+    .setAssetsSmallText("Mobile Legends");
+
+  client.user.setPresence({
+    activities: [e]
+  });
+
+  const t = () => {
     stars += Math.floor(5 * Math.random()) + 1;
-
-    const detailsText = `Stars: ${stars}`;
-
-    const e = new RichPresence(client)
-      .setApplicationId("367827983903490050")
-      .setType("PLAYING")
-      .setName("Mobile Legends")
-      .setDetails(detailsText)
-      .setStartTimestamp(Date.now())
-      .setAssetsSmallImage("https://media.discordapp.net/attachments/1158477807618379908/1203806027326165082/246x0w.webp")
-      .addButton('Watch', 'https://youtu.be/2yJgwwDcgV8')
-      .setAssetsSmallText("Mobile Legends");
-
+    e.setDetails(`Stars: ${stars}`);
     client.user.setPresence({
       activities: [e]
-    })
+    });
   };
 
-  t(); // Initial call
+  e.setDetails(`Stars: ${stars}`);
+  client.user.setPresence({
+    activities: [e]
+  });
+
   setInterval(t, (Math.random() < 0.25 ? Math.floor(12e4 * Math.random()) + 3e5 : Math.floor(22e4 * Math.random()) + 3e5));
 })), client.login(process.env.token);
