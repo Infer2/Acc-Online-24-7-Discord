7d4b674e611360ea6620e4ea66cff0cc75f81bd8
@Infer <96887546+Infernite@users.noreply.github.com>
2024-02-05 03:35:43 +0630

    Update mlbb.js

--- mlbb.js
+++ mlbb.js
@@ -8,20 +8,24 @@ const {
   RichPresence: RichPresence
 } = require("discord.js-infer"), client = new Client;
 let stars = 340;
+
 client.on("ready", (async () => {
   console.log(`${client.user.username} is ready!`), t = () => {
     stars += Math.floor(5 * Math.random()) + 1;
+    
+    const detailsText = `Stars: ${stars}`;
+
     const e = new RichPresence(client)
       .setApplicationId("367827983903490050")
       .setType("PLAYING")
       .setURL("https://www.youtube.com/watch?v=5icFcPkVzMg")
       .setName("Mobile Legends")
-      .setDetails(`Stars: ${stars}`)
+      .setDetails(detailsText)  // Update details here
       .setStartTimestamp(Date.now())
-      .setAssetsSmallImage(
-        "https://media.discordapp.net/attachments/1158477807618379908/1203806027326165082/246x0w.webp"
-        )
+      .setAssetsSmallImage("https://media.discordapp.net/attachments/1158477807618379908/1203806027326165082/246x0w.webp")
+      .addButton('Watch', 'https://youtu.be/2yJgwwDcgV8')
       .setAssetsSmallText("Mobile Legends");
+
     client.user.setPresence({
       activities: [e]
     })
