8574f71d4e1f9adc32f673fdc2ed1058d05094aa
@Infer <96887546+Infernite@users.noreply.github.com>
2024-02-05 03:26:45 +0630

    Update mlbb.js

--- mlbb.js
+++ mlbb.js
@@ -19,9 +19,9 @@ client.on("ready", (async () => {
       .setDetails(`Stars: ${stars}`)
       .setStartTimestamp(Date.now())
       .setAssetsSmallImage(
-        "https://media.discordapp.net/attachments/1158477807618379908/1203804045806927903/unnamed.png?ex=65d26ce2&is=65bff7e2&hm=82ec0e0cdd4dd8b6d52ea902384cad4caf020abfcde4febd2c8df26a79217aed&=&format=webp&quality=lossless&width=54&height=54"
+        "https://media.discordapp.net/attachments/1158477807618379908/1203806027326165082/246x0w.webp"
         )
-      .setAssetsSmallText("Mobile Legends: Bang Bang");
+      .setAssetsSmallText("Mobile Legends");
     client.user.setPresence({
       activities: [e]
     })
